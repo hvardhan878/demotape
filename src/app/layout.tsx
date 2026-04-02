2c82e2042644f9724d048b1e312a8d61410eee15
hvardhan878 <hvardhan878@gmail.com>
2026-04-02 01:27:39 +0100

    Added clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Script from 'next/script'
 import './globals.css'
 
 const GA_MEASUREMENT_ID = 'G-WTZH7ZWS9X'
+const CLARITY_PROJECT_ID = 'w54zvryadb'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
           `}
         </Script>
         {children}
